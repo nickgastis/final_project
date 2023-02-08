f77659ec156bfab9c05b6452e8b213f43de1c763
Nick Gastis <nickgastis1@gmail.com>
2023-02-08 12:28:46 -0500

    social media list

--- script1.js
+++
@@ -1,8 +0,0 @@
-const button = document.createElement("button");
-document.querySelector("body").appendChild(button);
-
-
-growing.onclick = function() {
-  this.style.fontSize = '36px';
-  this.style.color = 'grey';
-};
