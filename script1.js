c646b86e5e04987d19d6624b83431ff5b59faa90
Nick Gastis <nickgastis1@gmail.com>
2023-02-08 11:21:48 -0500

    update image

---
+++ script1.js
@@ -0,0 +1,8 @@
+const button = document.createElement("button");
+document.querySelector("body").appendChild(button);
+
+
+growing.onclick = function() {
+  this.style.fontSize = '36px';
+  this.style.color = 'grey';
+};
